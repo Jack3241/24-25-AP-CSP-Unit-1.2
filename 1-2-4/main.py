92c39da3dc778c807f4adf231ae81467bbc537d8
Jack <jack.turner@vtsd.com>
2024-12-04 11:45:22 -0500

    step 8-13

--- 1-2-4/main.py
+++ 1-2-4/main.py
@@ -22,23 +22,16 @@ def draw_barrier():
 
 
 for wall in range(21):
-    if (wall > 5):
+        maze_painter.forward(length/3)
+        maze_painter.penup()
+        maze_painter.forward(path_width)
+        maze_painter.pendown()
+        if (wall > 4):
+            draw_barrier()
+        maze_painter.forward(length - path_width - (length/3))
         maze_painter.left(90)
-        # randomize location of doors and barriers in wall
-        door = rand.randint(path_width * 2, (length - path_width * 2))
-        barrier = rand.randint(path_width * 2, (length - path_width * 2))
+        length += 15
 
-'''
-    maze_painter.forward(length/3)
-    maze_painter.penup()
-    maze_painter.forward(path_width)
-    maze_painter.pendown()
-    if (wall > 5):
-        draw_barrier()
-    maze_painter.forward(length - path_width - (length/3))
-    maze_painter.left(90)
-    length += 15
-'''
 
 wn.listen()
 wn.mainloop()
