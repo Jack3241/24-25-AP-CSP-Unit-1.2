fa8b6c5657bcfd3542ff6263640fbe5b94feb8b1
Jack <jack.turner@vtsd.com>
2024-12-10 11:41:21 -0500

    step 8-13

--- 1-2-5/main.py
+++ 1-2-5/main.py
@@ -3,6 +3,25 @@ import turtle as trtl
 import random as rand
 
 apple_image = "apple.gif"
+basket_image = "basket.jpg"
 
 wn = trtl.Screen()
+wn.setup(width=1, height=1)
+#make screen aware of new files
+wn.addshape(apple_image)
+wn.addshape(basket_image)
 wn.bgpic("background.gif")
+
+apple = trtl.Turtle()
+apple.penup()
+wn.tracer(False)
+
+def draw_apple(active_apple):
+    active_apple.shape(apple_image)
+    apple.showturtle()
+    wn.update()
+
+
+
+
+wn.mainloop()
