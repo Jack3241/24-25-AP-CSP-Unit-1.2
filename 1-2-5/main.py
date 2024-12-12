b88c9e12755e73db4fe903acde5dac9a5bc23012
Jack <jack.turner@vtsd.com>
2024-12-12 13:12:05 -0500

    project day 3

--- 1-2-5/main.py
+++ 1-2-5/main.py
@@ -7,6 +7,7 @@ basket_image = "basket.gif"
 xcor = 0
 ycor = 50
 ground_height = -200
+basket_height = -180
 
 wn = trtl.Screen()
 wn.setup(width=2, height=2)
@@ -15,13 +16,21 @@ wn.addshape(apple_image)
 wn.addshape(basket_image)
 wn.bgpic("background.gif")
 
+# set score up to left corner a screen
+score_writer = trtl.Turtle()
+score_writer.hideturtle()
+score_writer.penup()
+score_writer.goto(220, 250)
+score_writer.pendown()
+
+font_setup = ("Arial", 20, "normal")
+score = 0
+direction_list = "a" "d"
 apple = trtl.Turtle()
 apple.penup()
-wn.tracer(False)
 
 basket = trtl.Turtle()
 basket.penup()
-wn.tracer(False)
 
 def draw_apple(active_apple):
     active_apple.shape(apple_image)
@@ -32,23 +41,36 @@ def draw_basket(active_basket):
     active_basket.shape(basket_image)
     basket.showturtle()
     wn.update()
+    basket.speed(0)
+    basket.goto(0, basket_height)
 
 def drop_apple():
-    wn.tracer(True)
+    apple.speed(2)
     apple.goto(xcor, ground_height)
     apple.clear()
     apple.hideturtle()
-    wn.tracer(False)
-    reset_apple(apple)
 
-def reset_apple(apple):
-    if apple < ground_height:
-        newx = rand.randint(-200, 200)
-        newy = 0
-        apple.goto(newx, newy)
 
+def basket_left():
+    basket.goto(-200,basket_height )
+
+def basket_right():
+    basket.goto(200,basket_height)
+
+def basket_move_left():
+    if ("a" in direction_list):
+        basket_left()
+def basket_move_right():
+    if ("d" in direction_list):
+        basket_right()
+
+
+wn.onkeypress(basket_move_left, "a")
+wn.onkeypress(basket_move_right, "d")
 
 draw_apple(apple)
 draw_basket(basket)
 drop_apple()
+
+wn.listen()
 wn.mainloop()
